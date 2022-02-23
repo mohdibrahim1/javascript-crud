40d3cb90231f3810158c919e3d2db43b89408554
mohdibrahim1 <mohdi9003@gmail.com>
2022-02-23 08:07:48 -0400

    Initial commit

---
+++ data.js
@@ -0,0 +1,8 @@
+var marks = [
+    { id: 1, stuId: 101, english: 34, hindi: 59, maths: 60 },
+    { id: 2, stuId: 102, english: 44, hindi: 69, maths: 70 },
+    { id: 3, stuId: 3, english: 54, hindi: 79, maths: 80 },
+    { id: 4, stuId: 4, english: 64, hindi: 89, maths: 90 },
+]
+
+// console.log(marks);
